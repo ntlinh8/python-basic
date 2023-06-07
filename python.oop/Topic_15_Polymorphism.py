9bc91e48a4992242ea8646d73d5fd10a128bdcd0
Sunny Nguyen <64420667+ntlinh8@users.noreply.github.com>
2023-06-07 12:46:32 +0700

    Update Topic_15_Polymorphism.py

--- python.oop/Topic_15_Polymorphism.py
+++ python.oop/Topic_15_Polymorphism.py
@@ -1,7 +1,7 @@
-# The Polymorphism is the provision of a single interface for all class which was inheritanced it
+# The Polymorphism is the provision of a single interface for all class which was inherited it
 # Like the example in previous session
 # So, I'll create the base class with the base method (but cannot implement)
-# When the child class inheritances this class, they will implement this methods
+# When the child class inherits this class, they will implement this methods
 # Depend on the child class, the action in this method will be created
 # So, in the child class, we don't need define a new method, we only access to this method and implement it
 class Vehicle:
