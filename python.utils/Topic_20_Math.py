007d2cec3aa58587152b3c0466e559db8bca4ee4
Sunny Nguyen <64420667+ntlinh8@users.noreply.github.com>
2023-06-07 13:30:59 +0700

    Create Topic_20_Math.py

---
+++ python.utils/Topic_20_Math.py
@@ -0,0 +1,30 @@
+import math
+
+# get min max in the list
+print(min(1, 4, 3))
+print(max(1, 4, 3))
+
+# get the abs of the number
+print(abs(-7.12))
+
+# get the value of 4 to the power of 3 (same as 4*4*4)
+print(pow(2, 7))
+
+# get the square root of a number
+print(math.sqrt(81))
+
+# round a number upward to its nearest integer
+print(math.ceil(2.4))
+
+# round a number downwards to its nearest integer
+print(math.floor(2.4))
+
+# get pi constant
+print(math.pi)
+
+# get the value of sin, cos function
+print(math.sin(90))
+print(math.cos(0))
+print(math.tan(45))
+
+
